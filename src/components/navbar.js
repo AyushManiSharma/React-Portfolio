96f474e5d84753541ec91d3803252208754a0a01
Ayush <ayushmanisharma@gmail.com>
2023-10-09 22:11:00 -0400

    update

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './styles/navbar.css';
+import '../styles/navbar.css';
 
 function Navbar() {
   const [activePage, setActivePage] = useState('');
